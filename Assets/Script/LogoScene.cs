1081c3972bb3e771a0dea473e39c8418a4074f0c
OttoHawkins <djundeadvsmaxa@gmail.com>
2025-02-10 15:17:07 +0300

    fix script

--- Assets/Script/LogoScene.cs
+++ Assets/Script/LogoScene.cs
@@ -6,29 +6,30 @@ using System.Collections;
 public class LogoScene : MonoBehaviour
 {
     public Text companyNameText; 
+
     public float displayDuration = 10.0f; 
-    public float letterDelay = 0.1f; 
-    private string companyName = "Торресов пролив\r\n 28 августа 1791\r\n"; 
+    public float letterDelay = 0.1f;
+    private string companyName = "Торресов пролив\r\n 28 августа 1791\r\n";
     private bool textDisplayed = false;
 
-    public Text loadingText; 
-    private string loadingMessage = "Загрузка"; 
+    public Text loadingText;
+    private string loadingMessage = "Загрузка";
 
-    public AudioClip displaySound; 
+    public AudioClip displaySound;
     private AudioSource audioSource;
 
     void Start()
     {
         audioSource = gameObject.AddComponent<AudioSource>(); 
-        loadingText.gameObject.SetActive(true);
+        loadingText.gameObject.SetActive(true); 
         StartCoroutine(AnimateText()); 
-        StartCoroutine(AnimateLoadingText()); 
+        StartCoroutine(AnimateLoadingText());
     }
 
     IEnumerator AnimateText()
     {
-      
-        companyNameText.text = ""; 
+
+        companyNameText.text = "";
         foreach (char letter in companyName.ToCharArray())
         {
             companyNameText.text += letter; 
@@ -36,27 +37,27 @@ public class LogoScene : MonoBehaviour
             yield return new WaitForSeconds(letterDelay); 
         }
 
-   
+      
         yield return new WaitForSeconds(0.5f);
 
      
         textDisplayed = true;
         yield return new WaitForSeconds(displayDuration);
 
-
         for (float t = 1; t > 0; t -= Time.deltaTime)
         {
             Color color = companyNameText.color;
-            color.a = t;
+            color.a = t; 
             companyNameText.color = color;
-            yield return null;
+            yield return null; 
         }
 
-   
+ 
         loadingText.gameObject.SetActive(false);
 
-  
-        SceneManager.LoadScene("Lvl1"); 
+
+        SceneManager.LoadScene("Lvl1");
+    }
 
     IEnumerator AnimateLoadingText()
     {
